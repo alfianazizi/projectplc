ccde03881821315f9599fb6b5b9dfd478eb4ee0f
Alfian Azizi <alfianazizi4869@gmail.com>
2018-10-23 11:47:41 +0700

    updated filename and script

--- temp_v1.py
+++ temp_v1.py
@@ -5,12 +5,13 @@ import os
 
 cpu = CPUTemperature()
 header = ['time', 'temperature']
-
+filename_date = strftime("%Y-%m")
+filename_temp = filename_date + '-temp.csv'
 updateTemp = 5 #5 second interval update
 tt = time() #temp initial timer
 
 print("Logging Temperature")
-
+print(filename_date)
 
 def get_temp_data():
 	temp_data = []
@@ -19,19 +20,17 @@ def get_temp_data():
 
 	return temp_data
 
-def write_temp(data):
-	with open('log.csv', 'a') as log:
-		file_is_empty = os.stat('log.csv').st_size == 0
+def write_temp(filename,data):
+	with open(filename, 'a') as log:
+		file_is_empty = os.stat(filename).st_size == 0
 		temp_writer = writer(log, lineterminator='\n')
 		if file_is_empty:	
 			temp_writer.writerow(header)
 		temp_writer.writerow(data)
 
-
-
 while True:
 	data = get_temp_data()
 	t1 = time()
 	if t1 - tt >= updateTemp:
-		write_temp(data)
+		write_temp(filename_temp, data)
 		tt = time()
