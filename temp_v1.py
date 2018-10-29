59a4f6691b8d89a56451ee44172a062caf197649
Azizi <alfianazizi4869@gmail.com>
2018-10-29 10:39:15 +0700

    updated script logging to 1 file

--- temp_v1.py
+++ temp_v1.py
@@ -7,12 +7,13 @@ import os
 import random
 
 cpu = CPUTemperature()
-header_temp = ['time', 'temperature']
+header_temp = ['time', 'temperature', 'arus', 'battery']
 dirpath = os.path.dirname(os.path.realpath(__file__))
 filename_date = strftime("%Y-%m")
-filename_temp = dirpath + '/log/' + filename_date + '-temp.csv'
-filename_current = dirpath + '/log/' + filename_date + '-current.csv'
-filename_voltage = dirpath + '/log/' + filename_date + '-voltage.csv'
+# filename_temp = dirpath + '/log/' + filename_date + '-temp.csv'
+# filename_current = dirpath + '/log/' + filename_date + '-current.csv'
+# filename_voltage = dirpath + '/log/' + filename_date + '-voltage.csv'
+filename_log = dirpath + '/log/' + filename_date + '.csv'
 filename_sensor = dirpath + '/log/sensor-now.txt'
 updateSensor = 300 #second interval update
 tt = time() #temp initial timer
@@ -20,14 +21,16 @@ tn = time()
 
 print("Logging Temperature")
 
-def get_temp_data():
-	temp_data = []
-	temp_data.append(strftime("%Y-%m-%d %H:%M:%S"))
-	temp_data.append(cpu.temperature)
+def get_sensor_data(temperature, current, voltage):
+	sensor_data = []
+	sensor_data.append(strftime("%Y-%m-%d %H:%M:%S"))
+	sensor_data.append(temperature)
+	sensor_data.append(current)
+	sensor_data.append(voltage)
 
-	return temp_data
+	return sensor_data
 
-def write_temp(filename,data):
+def write_sensor(filename, data):
 	with open(filename, 'a') as log:
 		file_is_empty = os.stat(filename).st_size == 0
 		temp_writer = writer(log, lineterminator='\n')
@@ -35,39 +38,38 @@ def write_temp(filename,data):
 			temp_writer.writerow(header_temp)
 		temp_writer.writerow(data)
 
-def write_current(filename,data):
-	with open(filename, 'a') as log:
-		file_is_empty = os.stat(filename).st_size == 0
-		current_writer = writer(log, lineterminator='\n')
-		if file_is_empty:
-			current_writer.writerow(header_current)
-		current_writer.writerow(data)
+# def write_current(filename,data):
+# 	with open(filename, 'a') as log:
+# 		file_is_empty = os.stat(filename).st_size == 0
+# 		current_writer = writer(log, lineterminator='\n')
+# 		if file_is_empty:
+# 			current_writer.writerow(header_current)
+# 		current_writer.writerow(data)
 
-def write_voltage(filename,data):
-	with open(filename, 'a') as log:
-		file_is_empty = os.stat(filename).st_size == 0
-		voltage_writer = writer(log, lineterminator='\n')
-		if file_is_empty:
-			voltage_writer.writerow(header_voltage)
-		voltage_writer.writerow(data)
+# def write_voltage(filename,data):
+# 	with open(filename, 'a') as log:
+# 		file_is_empty = os.stat(filename).st_size == 0
+# 		voltage_writer = writer(log, lineterminator='\n')
+# 		if file_is_empty:
+# 			voltage_writer.writerow(header_voltage)
+# 		voltage_writer.writerow(data)
 
 def sensor_now(filename, temp, current, voltage):
 	with open(filename, 'w') as now:
 		now.write(str(temp) + '\n')
 		now.write(str(current) + '\n')
-		now.write(str(voltage))
+		now.write(str(voltage) + '\n')
 
 
 while True:
-	temp = get_temp_data()
+	temp = cpu.temperature
 	curr = random.randint(10,80)
 	volt = random.randint(60,100)
+	sensorData = get_sensor_data(temp, curr, volt)
 	t1 = time()
 	t2 = time()
 	if t1 - tt >= updateSensor:
-		write_temp(filename_temp, temp)
-		write_current(filename_current, curr)
-		write_voltage(filename_voltage, volt)
+		write_sensor(filename_log, sensorData)
 		tt = time()
 
 	if t2 - tn >= 5:
