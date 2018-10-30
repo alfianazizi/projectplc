447c12d7e034c035e5ec702bcf1d78be85e403cb
Balqis <sitibalqis@rocketmail.com>
2018-10-30 14:23:03 +0700

    Update writeFile.php

--- writeFile.php
+++ writeFile.php
@@ -2,11 +2,18 @@
 	if(isset($_POST['resp']))
 	{
 	    $conf = $_POST['resp'];
-	    // echo "<script>console.log('".json_encode($conf)."')</script>";
+	    $ip = 0;
+	    $c = 0;
+	    while ($ip == 0) {
+	    	if(substr($conf[$c], 0, 17) == "static ip_address") {
+	    		$ip = substr($conf[$c], 18, 36);
+	    	}
+	    	$c++;
+	    }
 
 	    // write to file
 	    file_put_contents("../../../etc/dhcpcd.conf", implode(PHP_EOL, $conf));
 
-	    echo ("Rebooting...");
+	    echo ("IP Address changed to ".$ip.". Rebooting now...");
 	}
 ?>
