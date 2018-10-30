4d17fbe03feec929005cce611a9d003ede841f94
Balqis <sitibalqis@rocketmail.com>
2018-10-30 13:01:04 +0700

    add readfile.php

---
+++ readFile.php
@@ -0,0 +1,23 @@
+<?php
+	$lineArr = [];
+	$counter = 0;
+	if ($file = fopen("../../etc/dhcpcd.conf", "r")) {
+		while (!feof($file)) {
+	        // if (fgets($file) != "\r\n") {
+		        $lineArr[$counter] = fgets($file);
+		        $counter++;
+		    // }
+	    }
+	    fclose($file);
+	}
+
+	// foreach ($lineArr as $key => $value) {
+		// if($value != "\n") {
+		// echo $value;
+	// }
+	// echo "<script>console.log('".print_r($lineArr)."')</script>";
+	// echo "<script>console.log('".print_r($arr)."')</script>";
+
+	echo json_encode($lineArr);
+
+?>
